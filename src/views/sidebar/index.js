275e3964fafa5ce241abdd33fcdee09191604f51
bvinicius <vinicius.bazanella1@gmail.com>
2019-09-02 18:42:43 -0300

    puxar dados da api

--- src/views/sidebar/index.js
+++ src/views/sidebar/index.js
@@ -3,17 +3,41 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
+import sidebar from '../../api/sidebar';
+
 class SidebarView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      instituicoes: []
+    };
+  }
+
+  getInstituicoes = async () => {
+    const res = await sidebar.get('instituicoes', {
+      params: {
+        latitude: 0,
+        longitude: 0
+      }
+    });
+
+    this.setState({
+      instituicoes: res.instituicoes
+    });
+  };
+
   render() {
     return (
       <div className="some-class">
         <List component="nav" aria-label="main mailbox folders">
-          {[0, 1, 2, 3, 4, 5, 6, 7].map(value => (
-            <ListItem button key={value}>
-              <ListItemText primary="Inbox" />
+          {this.state.instituicoes.map(instituicao => (
+            <ListItem button key={instituicao.id}>
+              <ListItemText primary={instituicao.nome} />
             </ListItem>
           ))}
         </List>
+        <button onClick={this.getInstituicoes}>teste</button>
       </div>
     );
   }
