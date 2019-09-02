578a836b1d123ad3f95f173474814056737465ea
Vitor Edgar <vitor.edgar@acad.pucrs.br>
2019-09-02 19:09:18 -0300

    9/@VEdgar,@Rabelo: Transformando em container e ajustes nos grids

--- src/App.js
+++ src/App.js
@@ -1,50 +1,13 @@
 import React from 'react';
 import './App.css';
-import { Grid, Paper } from '@material-ui/core';
 import DetalheInstituicaoContainer from './containers/DetalheInstituicaoContainer';
 
 function App() {
   return (
-    <Grid id justify="center" container spacing={12}>
-      <Grid item xs={12}>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <DetalheInstituicaoContainer
-              titulo={'Fundação Iberê Camargo'}
-              endereco={'Av. Padre Cacique'}
-            ></DetalheInstituicaoContainer>
-          </Grid>
-          {/* A completar abaixo */}
-          <Grid item xs={5}>
-            <Paper style={{ background: 'silver' }}>
-              Horários
-              <p>DOM 00:00 - 00:00</p>
-              <p>SEG 00:00 - 00:00</p>
-              <p>TER 00:00 - 00:00</p>
-              <p>QUA 00:00 - 00:00</p>
-              <p>QUI 00:00 - 00:00</p>
-              <p>SEX 00:00 - 00:00</p>
-              <p>SAB 00:00 - 00:00</p>
-            </Paper>
-          </Grid>
-          <Grid container spacing={2} item xs={7}>
-            <Grid item xs={12}>
-              A Fundação Iberê Camargo, sediada na cidade brasileira de Porto
-              Alegre, é uma entidade cultural que tem como objetivos a
-              preservação, o estudo e a divulgação da obra do pintor gaúcho
-              Iberê Camargo (1914-1994).
-            </Grid>
-            <Grid item xs={12}>
-              <p>E-mail:</p>
-              <p>Site:</p>
-            </Grid>
-          </Grid>
-          <Grid item xs={12}>
-            vet items
-          </Grid>
-        </Grid>
-      </Grid>
-    </Grid>
+    <DetalheInstituicaoContainer
+      titulo={'Fundação Ibere Camargo'}
+      endereco={'Avenida Padre Cacique, 1720'}
+    />
   );
 }
 
--- src/containers/DetalheInstituicaoContainer.js
+++ src/containers/DetalheInstituicaoContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Paper } from '@material-ui/core';
 
 type Props = {
   titulo: '',
@@ -16,26 +16,99 @@ class DetalheInstituicaoContainer extends Component<Props> {
 
   render() {
     return (
-      <div>
-        <Grid container spacing={8}>
-          <Grid item xs={8}>
-            <h1>{this.props.titulo}</h1>
-            <div style={{ color: 'grey' }}>{this.props.endereco}</div>
-            <div style={{ color: 'green' }}>Aberto</div>
-          </Grid>
-          <Grid
-            container
-            direction="row"
-            justify="center"
-            alignItems="center"
-            item
-            xs={4}
-          >
-            <button style={style}>Rotas</button>
+      <Grid id justify="center" container spacing={12}>
+        <Grid item xs={12}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <div>
+                <Grid container spacing={8}>
+                  <Grid item xs={8}>
+                    <h1>{this.props.titulo}</h1>
+                    <div style={{ color: 'grey' }}>{this.props.endereco}</div>
+                    <div style={{ color: 'green' }}>Aberto</div>
+                  </Grid>
+                  <Grid
+                    container
+                    direction="row"
+                    justify="center"
+                    alignItems="center"
+                    item
+                    xs={4}
+                  >
+                    <button style={style}>Rotas</button>
+                  </Grid>
+                </Grid>
+                <hr />
+              </div>
+            </Grid>
+            {/* A completar abaixo */}
+            <Grid item xs={5}>
+              <Paper style={{ background: 'silver' }}>
+                <Grid>
+                  <p style={{ marginBottom: '0vh' }}>Horários</p>
+                </Grid>
+
+                <Grid container>
+                  <Grid item xs={4}>
+                    <p>DOM</p>
+                  </Grid>
+                  <Grid item xs={8}>
+                    <p>00:00 - 00:00</p>
+                  </Grid>
+                </Grid>
+                <Grid container>
+                  <Grid item xs={4}>
+                    <p>DOM</p>
+                  </Grid>
+                  <Grid item xs={8}>
+                    <p>00:00 - 00:00</p>
+                  </Grid>
+                </Grid>
+                <Grid container>
+                  <Grid item xs={4}>
+                    <p>DOM</p>
+                  </Grid>
+                  <Grid item xs={8}>
+                    <p>00:00 - 00:00</p>
+                  </Grid>
+                </Grid>
+                <Grid container>
+                  <Grid item xs={4}>
+                    <p>DOM</p>
+                  </Grid>
+                  <Grid item xs={8}>
+                    <p>00:00 - 00:00</p>
+                  </Grid>
+                </Grid>
+                <Grid container>
+                  <Grid item xs={4}>
+                    <p>DOM</p>
+                  </Grid>
+                  <Grid item xs={8}>
+                    <p>00:00 - 00:00</p>
+                  </Grid>
+                </Grid>
+              </Paper>
+            </Grid>
+
+            <Grid container spacing={2} item xs={7}>
+              <Grid item xs={12}>
+                A Fundação Iberê Camargo, sediada na cidade brasileira de Porto
+                Alegre, é uma entidade cultural que tem como objetivos a
+                preservação, o estudo e a divulgação da obra do pintor gaúcho
+                Iberê Camargo (1914-1994).
+              </Grid>
+              <Grid item xs={12}>
+                <p>E-mail:</p>
+                <p>Site:</p>
+              </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              vet items
+            </Grid>
           </Grid>
         </Grid>
-        <hr />
-      </div>
+      </Grid>
     );
   }
 }
