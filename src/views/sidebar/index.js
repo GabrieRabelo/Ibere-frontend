2c17e8166d7ab141415bad2b43420aef0d9329d6
Joao C Vieira <joaocv3@gmail.com>
2019-09-02 21:46:12 -0300

    feature: Organize institution details

--- src/views/sidebar/index.js
+++ src/views/sidebar/index.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
 
 class SidebarView extends Component {
   constructor(props) {
@@ -34,11 +37,29 @@ class SidebarView extends Component {
         <div className="some-class">
           <List component="nav" aria-label="main mailbox folders">
             {this.state.instituicoes.map(instituicao => (
-              <ListItem button key={instituicao.id}>
-                <ListItemText primary={instituicao.nome} />
-                <ListItemText primary={instituicao.endereco} />
-                <ListItemText primary={instituicao.aberto ? 'Sim' : 'Não'} />
+              <ListItem button alignItems="flex-start" key={instituicao.id}>
+                 <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="http://iberecamargo.org.br/wp-content/uploads/2018/07/Fachada-foto-Gustavo-Possamai.jpg" />
+        </ListItemAvatar>
+                <ListItemText
+                  primary={instituicao.nome}
+                  secondary={
+                    <React.Fragment>
+                      <Typography
+                        component="span"
+                        variant="body2"
+                        className="AS"
+                        color="textPrimary"
+                      >
+                        {instituicao.descricao}
+                      </Typography>
+                      {` ${instituicao.telefone} | Aberto agora: ${ instituicao.aberto ? "Sim" : "Não"} `}
+                    </React.Fragment>
+                  }
+                />
+
               </ListItem>
+
             ))}
           </List>
         </div>
