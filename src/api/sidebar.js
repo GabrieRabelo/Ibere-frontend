6a6d0db6148aac9b06fec8b29cbe4096e530febe
Joao C Vieira <joaocv3@gmail.com>
2019-09-02 21:25:12 -0300

    feature: Show sample data in screen

--- src/api/sidebar.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://my-json-server.typicode.com/joaocv3/sample_endpoint_ibere'
-});
--- src/views/sidebar/index.js
+++ src/views/sidebar/index.js
@@ -3,8 +3,6 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
-import sidebar from '../../api/sidebar';
-
 class SidebarView extends Component {
   constructor(props) {
     super(props);
@@ -12,34 +10,42 @@ class SidebarView extends Component {
     this.state = {
       instituicoes: []
     };
+
+    this.getInstituicoes();
   }
 
-  getInstituicoes = async () => {
-    const res = await sidebar.get('instituicoes', {
-      params: {
-        latitude: 0,
-        longitude: 0
-      }
-    });
-
-    this.setState({
-      instituicoes: res.instituicoes
-    });
+  getInstituicoes = () => {
+    fetch(
+      'https://my-json-server.typicode.com/joaocv3/sample_endpoint_ibere/instituicoes'
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({
+          instituicoes: data
+        });
+      });
   };
 
   render() {
-    return (
-      <div className="some-class">
-        <List component="nav" aria-label="main mailbox folders">
-          {this.state.instituicoes.map(instituicao => (
-            <ListItem button key={instituicao.id}>
-              <ListItemText primary={instituicao.nome} />
-            </ListItem>
-          ))}
-        </List>
-        <button onClick={this.getInstituicoes}>teste</button>
-      </div>
-    );
+    if (this.state.instituicoes) {
+      return (
+        <div className="some-class">
+          <List component="nav" aria-label="main mailbox folders">
+            {this.state.instituicoes.map(instituicao => (
+              <ListItem button key={instituicao.id}>
+                <ListItemText primary={instituicao.nome} />
+                <ListItemText primary={instituicao.endereco} />
+                <ListItemText primary={instituicao.aberto ? 'Sim' : 'Não'} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      );
+    } else {
+      return <div>Carregando info...</div>;
+    }
   }
 }
 
