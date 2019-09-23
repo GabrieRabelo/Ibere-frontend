ea0a40331c68a5c7b73c152cf5f643f2a2b5347c
Vitor Edgar da Silva <vitor.edgar@acad.pucrs.br>
2019-09-23 18:16:03 -0300

    Revert "Integraçao maps e pwa"
    This reverts commit e85ca6acd004183027f004dc528a48f593d7de2e

--- src/components/map/MapComponent.js
+++ src/components/map/MapComponent.js
@@ -1,65 +1,42 @@
 import React, { Component } from 'react';
 
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 
 import SidebarView from '../../components/sidebar/SidebarComponent';
-import { GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
-
-
-import CurrentLocation from '../currentLocation';
 
 class MapView extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      showingInfoWindow: false,
-      activeMarker: {},
-      selectedPlace: {}
-  };
-
-  }
-
-
-  onMarkerClick = (props, marker, e) =>
-  this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
-  });
-
-onClose = props => {
-  if (this.state.showingInfoWindow) {
-      this.setState({
-          showingInfoWindow: false,
-          activeMarker: null
-      });
-  }
-};
-
-
   render() {
     return (
       <Grid>
-            <CurrentLocation centerAroundCurrentLocation google={this.props.google}>
-                <Marker onClick={this.onMarkerClick} name={'current location'} />
-                <InfoWindow
-                    marker={this.state.activeMarker}
-                    visible={this.state.showingInfoWindow}
-                    onClose={this.onClose}
-                >
-                    <div>
-                        <h4>{this.state.selectedPlace.name}</h4>
-                    </div>
-                </InfoWindow>
-            </CurrentLocation>
+        <Card className="card">
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              alt="Contemplative Reptile"
+              height="600"
+              image="https://via.placeholder.com/900x700.png"
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                PLACEHOLDER
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
         <SidebarView />
       </Grid>
     );
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDvHSPp_Ov4xxoCtR7Rd1299bQ8hQYSWRI'
-})(MapView);
-
+export default MapView;
