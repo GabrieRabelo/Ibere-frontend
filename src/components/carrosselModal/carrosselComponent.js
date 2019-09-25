c797630e4529bbc78a76e9305d55bd7db1332563
gabriel.almeida <gabriel.rabelo@edu.pucrs.br>
2019-09-24 21:27:34 -0300

    25/@gabriel.almeida, @vbazanella: conclui parte do carrossel

--- src/components/carrosselModal/carrosselComponent.js
+++ src/components/carrosselModal/carrosselComponent.js
@@ -4,21 +4,20 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
 const responsive = {
-  superLargeDesktop: {
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5
-  },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3
+    items: 3,
+    partialVisibilityGutter: 80 // this is needed to tell the amount of px that should be visible.
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 2
+    items: 2,
+    partialVisibilityGutter: 80 // this is needed to tell the amount of px that should be visible.
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
-    items: 1
+    items: 1,
+    partialVisibilityGutter: 80 // this is needed to tell the amount of px that should be visible.
   }
 };
 
@@ -27,11 +26,13 @@ class CarouselComponent extends React.Component {
     return (
       <Carousel
         responsive={responsive}
+        swipeable={true}
         draggable={true}
         arrows={false}
         focusOnSelect={true}
         infinite={true}
         showDots={true}
+        partialVisbile={true}
       >
         {this.props.instituicao.imagens.map(imagem => (
           <div className="slide" key={imagem}>
