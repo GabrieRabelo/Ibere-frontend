5892f9b0b902790f6017c5abc51add00d22b1ce8
vbazanella <vinicius.bazanella1@gmail.com>
2019-10-10 22:02:12 -0300

    24/@gabriel.rabelo, @vinicius.bazanella: Define padrão inicial de responsividade para o carrossel

--- src/components/carrosselModal/carrosselComponent.js
+++ src/components/carrosselModal/carrosselComponent.js
@@ -1,35 +1,33 @@
 import React from 'react';
 
 import AliceCarousel from 'react-alice-carousel';
+import './carrosselComponent.css';
 import 'react-alice-carousel/lib/alice-carousel.css';
 
 class CarouselComponent extends React.Component {
-  images = this.props.instituicao.imagens.map(i => {
-    return <img className="imagem" src={i} key={i} alt={''} />;
+  images = this.props.instituicao.imagens.map(img => {
+    return <img className="imagem" src={img} key={img} alt={''} />;
   });
 
-  responsive = {
-    0: {
-      items: 1
-    },
-
-    350: {
-      items: 1.1
-    },
-
-    412: {
-      items: 1.2
-    }
-  };
+  responsive = { 
+    0: { items: 1.2 },
+    415: { items: 1.4 },
+    500: { items: 1.5 },
+    600: { items: 1.7 },
+    700: { items: 1.9 },
+    800: { items: 2.1 },
+    900: { items: 2.3 },
+    
+  }
 
   render() {
     return (
       <AliceCarousel
-        buttonsDisabled={true}
         items={this.images}
-        autoHeight={true}
+        buttonsDisabled
+        dotsDisabled
         responsive={this.responsive}
-      />
+        />
     );
   }
 }
