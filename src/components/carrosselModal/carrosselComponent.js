1fd1ba878f25c69aec3af8f19f60e1d4a1039830
vbazanella <vinicius.bazanella1@gmail.com>
2019-10-10 22:02:30 -0300

    24/@gabriel.rabelo, @vinicius.bazanella: Define padrão inicial de responsividade para o carrossel

--- src/components/carrosselModal/carrosselComponent.js
+++ src/components/carrosselModal/carrosselComponent.js
@@ -9,16 +9,15 @@ class CarouselComponent extends React.Component {
     return <img className="imagem" src={img} key={img} alt={''} />;
   });
 
-  responsive = { 
+  responsive = {
     0: { items: 1.2 },
     415: { items: 1.4 },
     500: { items: 1.5 },
     600: { items: 1.7 },
     700: { items: 1.9 },
     800: { items: 2.1 },
-    900: { items: 2.3 },
-    
-  }
+    900: { items: 2.3 }
+  };
 
   render() {
     return (
@@ -27,7 +26,7 @@ class CarouselComponent extends React.Component {
         buttonsDisabled
         dotsDisabled
         responsive={this.responsive}
-        />
+      />
     );
   }
 }
