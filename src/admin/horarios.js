d564fdce4cc193e8afed776418760c47db909f58
Joao C Vieira <joaocv3@gmail.com>
2019-11-18 18:50:31 -0300

    60/@joao.vieira: Mostra horarios apenas quando disponível

--- src/admin/horarios.js
+++ src/admin/horarios.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 export const Horarios = ({ record = {} }) => {
+  if (!record.horarios) {
+    return null;
+  }
+
   return record.horarios.map(horario => {
     return (
       <p key={horario.dia}>
-        {`${horario.dia}: ${horario.horario_abertura} - ${horario.horario_encerramento}`}
+        {`${horario.dia}: ${horario.horarioAbertura} - ${horario.horarioEncerramento}`}
       </p>
     );
   });
