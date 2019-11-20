3f36db47e509235f48b6c0136bef81845d9ff697
Francisco João Lucca Neto <franciscojoaoluccaneto@gmail.com>
2019-11-19 21:33:09 -0300

    54/@Francisco: Correcao dos conflitos de MR

--- src/components/descricaoInstituicao/DescricaoInstituicaoComponent.js
+++ src/components/descricaoInstituicao/DescricaoInstituicaoComponent.js
@@ -1,19 +1,15 @@
 import React from 'react';
 
-import { Grid, Typography} from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 
 import './DescricaoInstituicao.css';
 
-
-
 const DescricaoInstituicaoComponent = props => {
-
- 
   return (
-    <Grid container className="descricaoContainer">      
-        <Typography className="descricao" variant="body1">
-          {props.descricao}
-        </Typography>
+    <Grid container className="descricaoContainer">
+      <Typography className="descricao" variant="body1">
+        {props.descricao}
+      </Typography>
     </Grid>
   );
 };
